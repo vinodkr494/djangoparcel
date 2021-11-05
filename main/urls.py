55831fca2b2f0464bb39dfa134647bd58d44ccac
vinod kumar <vinod.globalolympiad@gmail.com>
2021-11-05 15:30:08 +0530

    initial commit

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from main.views import *
+app_name = 'main'
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('customer', customer_page, name='customer_page'),
+    path('courier', courier_page, name='courier_page'),
+]
---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home(request):
+    template = 'home.html'
+    return render(request, template)
+
+
+def customer_page(request):
+    template = 'customer.html'
+    return render(request, template)
+
+def courier_page(request):
+    template = 'courier.html'
+    return render(request, template)
